f2b9cc3eb005d216f42e06b20698925e898b919a
christian <christianbach1689@googlemail.com>
2025-02-01 15:44:57 +0100

    first request

--- app/src/main/java/com/bach/familyfresh/features/actualmenu/screens/ActualMenuScreen.kt
+++ app/src/main/java/com/bach/familyfresh/features/actualmenu/screens/ActualMenuScreen.kt
@@ -1,10 +1,14 @@
 package com.bach.familyfresh.features.actualmenu.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,6 +21,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bach.familyfresh.features.actualmenu.bars.BottomAppBar
 import com.bach.familyfresh.features.actualmenu.bars.TopAppBar
+import com.bach.familyfresh.features.actualmenu.viewmodel.ActualMenuScreenStatus
 import com.bach.familyfresh.features.actualmenu.viewmodel.ActualMenuScreenViewModel
 import com.bach.familyfresh.features.actualmenu.views.MenuView
 import com.bach.familyfresh.ui.theme.FamilyFreshTheme
@@ -30,39 +35,41 @@ fun ActualMenuScreen(
     Scaffold(
         modifier = modifier,
         topBar = { TopAppBar() },
-        bottomBar = { BottomAppBar()
+        bottomBar = {
+            BottomAppBar()
         }
     ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            item {
-                Card(
-                    modifier = Modifier
-                        .padding(horizontal = 10.dp, vertical = 10.dp)
+        when (val uiState = actualMenuScreenViewModel.menus.value) {
+            is ActualMenuScreenStatus.loading ->
+                Column(
+                    Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    MenuView(
-                        "Gefüllte Paprikaschote",
-                        "mit Kartoffeln und Soße",
-                        listOf("Schwein", "Express"),
-                        "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/E829412C-6272-4361-ADC1-DD016CBB03C1/Derivates/B0FA5272-D8BB-4E16-B56A-B9717A651432.jpg"
-                    );
+                    CircularProgressIndicator()
                 }
-            }
-            item {
-                Card(
+
+            is ActualMenuScreenStatus.error -> Text("Error")
+            is ActualMenuScreenStatus.success -> {
+                LazyColumn(
                     modifier = Modifier
-                        .padding(horizontal = 10.dp, vertical = 10.dp)
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    MenuView(
-                        "Nudeln",
-                        "mit Tomatensauce",
-                        listOf("Vegetarisch"),
-                        "https://static.bremer-gewuerzhandel.de/media/0a/83/48/1725976897/nudeln-mit-hackfleisch-00779-00789-00072-01304.webp"
-                    );
+                    items(uiState.menus) { menu ->
+                        Card(
+                            modifier = Modifier
+                                .padding(horizontal = 10.dp, vertical = 10.dp)
+                        ) {
+                            MenuView(
+                                menu.title ?: "",
+                                subTitle = "",
+                                labels = menu.labels?.map { label -> label.name } ?: emptyList(),
+                                imageUrl = menu.image ?: ""
+                            )
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/bach/familyfresh/features/actualmenu/viewmodel/ActualMenuScreenViewModel.kt
+++ app/src/main/java/com/bach/familyfresh/features/actualmenu/viewmodel/ActualMenuScreenViewModel.kt
@@ -1,29 +1,45 @@
 package com.bach.familyfresh.features.actualmenu.viewmodel
 
 import android.util.Log
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bach.familyfresh.data.MenuRepository
 import kotlinx.coroutines.launch
+import org.openapitools.client.infrastructure.HttpResponse
+import org.openapitools.client.models.RecipeReadDto
+
+sealed interface ActualMenuScreenStatus {
+    data object loading: ActualMenuScreenStatus
+    data class success(var menus: List<RecipeReadDto>) : ActualMenuScreenStatus
+    data class error(var error: Throwable, var errorMsg : String) : ActualMenuScreenStatus
+
+}
+
 
 class ActualMenuScreenViewModel(private val menuRepository: MenuRepository = MenuRepository()) : ViewModel() {
 
-
+    val menus : MutableState<ActualMenuScreenStatus> =  mutableStateOf(ActualMenuScreenStatus.loading)
 
     init {
         getActualMenu()
     }
 
-
-     fun getActualMenu() {
+    private fun getActualMenu() {
         viewModelScope.launch {
-            var recipe = menuRepository.getActualMenu().body().get(0)
-            Log.d("ActualMenuScreenViewModel","Recipe: "+recipe.title);
-
+            try {
+                val response = menuRepository.getActualMenu();
+                if(response.success) {
+                    val responseData = response.body();
+                    menus.value = ActualMenuScreenStatus.success(listOf( responseData[0],responseData[1]))
+                    Log.d("ActualMenuScreenViewModel","Recipe: "+responseData[0].title);
+                }
+            } catch(error: Throwable) {
+                ActualMenuScreenStatus.error(error,"Error fetching data");
+            }
         }
     }
-
-
-
-
 }
